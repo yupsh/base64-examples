f6358dd0a2a6ae95a40db7aaa53200af84afe1b4
nicerobot <nicerobot@users.noreply.github.com>
2025-11-08 17:51:11 -0800

    tests

--- decode_test.go
+++ decode_test.go
@@ -3,16 +3,15 @@ package base64_test
 import (
 	"strings"
 
-	yup "github.com/gloo-foo/framework"
+	gloo "github.com/gloo-foo/framework"
 	. "github.com/yupsh/base64"
 )
 
 func ExampleBase64_decode() {
 	// echo "SGVsbG8gV29ybGQ=" | base64 -d
-	yup.MustRun(
+	gloo.MustRun(
 		Base64(Decode, strings.NewReader("SGVsbG8gV29ybGQ=")),
 	)
 	// Output:
 	// Hello World
 }
-
--- encode_test.go
+++ encode_test.go
@@ -3,16 +3,15 @@ package base64_test
 import (
 	"strings"
 
-	yup "github.com/gloo-foo/framework"
+	gloo "github.com/gloo-foo/framework"
 	. "github.com/yupsh/base64"
 )
 
 func ExampleBase64_encode() {
 	// echo "Hello World" | base64
-	yup.MustRun(
+	gloo.MustRun(
 		Base64(strings.NewReader("Hello World")),
 	)
 	// Output:
 	// SGVsbG8gV29ybGQ=
 }
-
